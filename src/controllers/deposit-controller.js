313cdcb5f37fd7f7c67ee7d71b18f5edcb38f30a
Silapakarn Saniskosai <silapakaneragon@hotmail.co.th>
2023-10-18 00:43:08 +0700

    update api create deposits

--- src/controllers/deposit-controller.js
+++ src/controllers/deposit-controller.js
@@ -1,4 +1,5 @@
 const { depositSchema } = require("../validators/deposit-validator");
+const constantCoin = require("../util/constant/coin")
 const constantStatus = require("../util/constant/status")
 const prisma = require("../models/prisma");
 const { response } = require("express");
@@ -8,10 +9,8 @@ const { response } = require("express");
 exports.create = async (req, res, next) => {
 
     try{
-        const { value, error } = depositSchema.validate(req.body);
-        if (error) {
-            return next(error);
-        }
+        const value = req.body;
+       
         //get username from Auth
         const userId = 1
 
@@ -22,6 +21,49 @@ exports.create = async (req, res, next) => {
 
         const createHistoryPayment = await prisma.history_payment.create({
             data: bodyRequest,
+        });
+
+        const findCoinListUSDT = await prisma.coin_list.findFirst({
+            where: {
+                coin_name: constantCoin.USDT
+            }
+        });
+
+        const newAmountUpdateInTableCoinList = parseFloat(findCoinListUSDT?.quantity) - parseFloat(value?.amount);
+        await prisma.coin_list.update({
+            where: {
+                coin_list_id: findCoinListUSDT?.coin_list_id
+            },
+            data: {
+                quantity: newAmountUpdateInTableCoinList
+            }
+        });
+
+
+        let bodyTransaction = {
+            coin_name: constantCoin.USDT,
+            type: constantStatus.BUY,
+            price: value?.amount,
+            quantity: value?.amount,
+            user_id: userId,
+            status: constantStatus?.ACTIVE
+        }
+        
+        await prisma.transaction.create({
+            data: bodyTransaction,
+        });
+
+        let bodyNewCoinInPortfolio = {
+            coin_name: constantCoin.USDT,
+            average_purchase_price: 0.00,
+            quantity: value?.amount,
+            profit_or_loss: 0.00,
+            weight: 0,
+            user_id: userId
+        }
+
+        await prisma.portfolio.create({
+            data: bodyNewCoinInPortfolio,
         });
 
         console.log("createHistoryPayment: --------> ", createHistoryPayment);
