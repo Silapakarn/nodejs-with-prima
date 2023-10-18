f4d18353aba643e9e15ad376358fbeac66b710f8
Silapakarn Saniskosai <silapakaneragon@hotmail.co.th>
2023-10-18 23:13:07 +0700

    update api  deposit create and update

--- src/controllers/deposit-controller.js
+++ src/controllers/deposit-controller.js
@@ -2,9 +2,7 @@ const { depositSchema } = require("../validators/deposit-validator");
 const constantCoin = require("../util/constant/coin")
 const constantStatus = require("../util/constant/status")
 const prisma = require("../models/prisma");
-const { response } = require("express");
-
-
+const constantFee = require("../util/constant/fee")
 
 exports.create = async (req, res, next) => {
 
@@ -39,12 +37,14 @@ exports.create = async (req, res, next) => {
             }
         });
 
-
+        const Fee = constantFee?.FEE
+        const fee = (parseFloat(value?.amount) * parseFloat(Fee))
         let bodyTransaction = {
             coin_name: constantCoin.USDT,
             type: constantStatus.BUY,
             price: value?.amount,
             quantity: value?.amount,
+            fee: fee,
             user_id: userId,
             status: constantStatus?.ACTIVE
         }
@@ -66,8 +66,13 @@ exports.create = async (req, res, next) => {
             data: bodyNewCoinInPortfolio,
         });
 
+        let response = {
+            status: constantStatus.SUCCEDD,
+            createHistoryPayment: createHistoryPayment
+        }
+
         console.log("createHistoryPayment: --------> ", createHistoryPayment);
-        res.status(201).json({ createHistoryPayment });
+        res.status(201).json({ response });
     }catch (err) {
         next(err);
     }
@@ -82,29 +87,87 @@ exports.update = async (req, res, next) => {
         }
 
         const userId = 1;
-        const findHistoryPayment = await prisma.history_payment.findFirst({
-            where: {
-                user_id: userId
-            }
-        });
 
-        if (!findHistoryPayment) {
-            return res.status(404).json({ message: 'History payment not found for the user.' });
+        let bodyRequest = {
+            amount: value?.amount,
+            user_id: userId,
         }
 
-        const newAmount = parseFloat(findHistoryPayment?.amount) + parseFloat(value?.amount);
+        await prisma.history_payment.create({
+            data: bodyRequest,
+        });
 
-        const updateHistoryPayment = await prisma.history_payment.update({
+
+        const findCoinListUSDT = await prisma.coin_list.findFirst({
             where: {
-                history_payment_id: findHistoryPayment?.history_payment_id 
-            },
-            data: {
-                amount: newAmount
+                coin_name: constantCoin.USDT
             }
         });
 
-        console.log("updateHistoryPayment: --------> ", updateHistoryPayment);
-        res.status(201).json({ updateHistoryPayment });
+        const newAmountUpdateInTableCoinList = parseFloat(findCoinListUSDT?.quantity) - parseFloat(value?.amount);
+        await prisma.coin_list.update({
+            where: {
+                coin_list_id: findCoinListUSDT?.coin_list_id
+            },
+            data: {
+                quantity: newAmountUpdateInTableCoinList
+            }
+        });
+
+
+        const Fee = constantFee?.FEE
+        const fee = (parseFloat(value?.amount) * parseFloat(Fee))
+        let bodyTransaction = {
+            coin_name: constantCoin.USDT,
+            type: constantStatus.BUY,
+            price: value?.amount,
+            quantity: value?.amount,
+            fee: fee,
+            user_id: userId,
+            status: constantStatus?.ACTIVE
+        }
+        await prisma.transaction.create({
+            data: bodyTransaction,
+        });
+
+        const findOldUsdtInPortfolio = await prisma.portfolio.findFirst({
+            where: {
+                AND: [{coin_name: constantCoin.USDT}, {user_id: userId}]
+            }
+        })
+
+        const newAmount = parseFloat(findOldUsdtInPortfolio?.quantity) + parseFloat(value?.amount);
+
+
+        const checkPortfolioRelationship = await prisma.portfolio.findFirst({
+            where: {
+                AND: [{coin_name: constantCoin.USDT},{user_id: userId}]
+            }
+        })
+  
+        await prisma.portfolio.update({
+            data: {
+                weight: 0,
+                average_purchase_price: 0,
+                profit_or_loss: 0,
+                quantity: newAmount,
+            },
+            where: {
+                portfolio_id: checkPortfolioRelationship.portfolio_id,
+                AND: [
+                    { user_id: userId },
+                    { coin_name: constantCoin.USDT }
+                ]
+            }
+        });
+
+        let response = {
+            status: constantStatus.SUCCEDD,
+            newAmount: newAmount
+        }
+
+        console.log("updateHistoryPayment: --------> Succeed !");
+        res.status(201).json({ response });
     } catch (err) {
         next(err);
     }
@@ -131,6 +194,7 @@ exports.validate = async (req, res, next) => {
         }
 
         const response = {
+            status: constantStatus.SUCCEDD,
             validate: validate
         }       
         console.log("validate: --------> ", response);
